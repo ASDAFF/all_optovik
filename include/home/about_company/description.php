209181a720490cbe6dd1733bc9b5ecd81a905877
DoNotEvenThink <alekseivasin1993@gmail.com>
2017-12-25 15:34:29 +0300

    add include "home->about_company"

---
+++ include/home/about_company/description.php
@@ -0,0 +1,3 @@
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus
+accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes,
+nascetur ridiculus mus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
