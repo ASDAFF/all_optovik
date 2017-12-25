1b2b053ff5f7297d0f34c07268433383cdf52980
OptovikTMWeb <za9bali_so_vzlomom>
2017-12-25 11:31:53 +0300

    feedback form in footer, create ajax & profile directories

--- local/templates/main/footer.php
+++ local/templates/main/footer.php
@@ -2,6 +2,28 @@
 if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
     die();
 ?>
+<?php
+/**
+ * @TODO make it visible only for need sections
+ */
+?>
+<? if(true) : ?>
+    <div class="container-fluid feedback__form">
+        <div class="container">
+            <div class="feedback__form__block">
+                <div class="feedback__form__title">
+                    <span>Будьте в курсе!</span>
+                </div>
+                <div class="feedback__form__input core__form__input">
+                    <input class="feedback__form__input__control core__form__input__control" placeholder="your@email.com">
+                </div>
+                <div class="feedback__form__btn">
+                    <a href="" title="" class="core__btn">подписаться</a>
+                </div>
+            </div>
+        </div>
+    </div>
+<? endif; ?>
 
 </div>
 <footer>
