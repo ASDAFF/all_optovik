98683166cb57fcb15cb3bc85e430898ca875458f
DoNotEvenThink <alekseivasin1993@gmail.com>
2017-12-25 15:33:51 +0300

    add template home slider

---
+++ local/templates/.default/components/bitrix/news.list/home_slider/template.php
@@ -0,0 +1,25 @@
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+
+if (empty($arResult['ITEMS']))
+    return;
+
+$data = new \Lema\Template\TemplateHelper($this);
+foreach ($data->items() as $item):?>
+    <a href="<?= $item->get('PROPERTY_LINK_VALUE'); ?>"
+       title="<?= $item->getName(); ?>"
+       class="block__item"
+       style="background-image: url('<?= $item->previewPicture(); ?>')" <?=$item->editId();?>></a>
+<?endforeach;
