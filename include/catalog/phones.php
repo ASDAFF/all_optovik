ab831e69146d503ef669069f443871a533004d1f
OptovikTMWeb <allptovik@marsha.timeweb.ru>
2018-01-16 15:27:18 +0300

    edit include file phone

--- include/catalog/phones.php
+++ include/catalog/phones.php
@@ -1,1 +1,4 @@
-<a class="catalog__detail__head__text__phone" href="tel:88002012020" title="">8-800-201-20-20</a>
+<p style="text-align:right;">
+<a class="catalog__detail__head__text__phone" href="tel:89137551505" title="">8-913-755-15-05</a>
+<a class="catalog__detail__head__text__phone" href="tel:89529029237" title="">8-952-902-92-37</a>
+</p>
